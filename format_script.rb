61abda3bfb722a83df6cafd6d3f70f5634eeb8b6
Elena Sharma <esharma@pivotal.io>
2016-01-22 14:57:04 -0800

    Fix format script
    [#111651002]
    
    Signed-off-by: Michael Trestman <mtrestman@pivotal.io>

--- format_script.rb
+++ format_script.rb
@@ -2,7 +2,7 @@
 cli_output = File.read('help.txt')
 
 cli_output.gsub! /\s*?$/, ''
-cli_output.gsub! (/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>" + x}
+cli_output.gsub! (/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>#{x}" }
 
 table_line_regex = /^\s{2,}([\w-].+?)\s{2,}(\w.+?)$/
 
@@ -15,11 +15,11 @@ table_regex = /(^##[^\n]*?$)(\n(^[^\n]+?\|[^\n]+?$))/
 
 cli_output.gsub!(table_regex){"#{$1}\nName   |   Description\n------------|------------#{$2}"}
 
-chunk_regex = /(## )(.*?):(.*?)(?=#)/m
+chunk_regex = /(##.*?\/a>)(.*?):(.*?)(?=#)/m
 
-cli_output.gsub!(chunk_regex){"#{$1}#{$2}\n#{$3}\n[Top](##{$2.downcase.gsub(' ','-')})\n"}
-# cli_output.gsub!(chunk_regex){"#{$1 + $2 + $3}"}
-# cli_output.gsub!(chunk_regex){"XXX"}
+cli_output.gsub!(chunk_regex){"#{$1}#{$2}#{$3}\n[Top](##{$2.downcase.gsub(' ','-')}"}
 
 
-File.open('help.txt', "w") { |file| file << cli_output }
+
+File.open('cli_help.html.md.erb', "w") { |file| file << cli_output }
+
