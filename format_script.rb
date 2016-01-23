d439c7b2ae9c2059c078e3132ac2cebcaefedd18
Elena Sharma <esharma@pivotal.io>
2016-01-22 17:56:21 -0800

    Adds multiple languages
    [#111651002]
    
    Signed-off-by: Michael Trestman <mtrestman@pivotal.io>

--- format_script.rb
+++ format_script.rb
@@ -1,5 +1,15 @@
-`cf help > help.txt`
-cli_output = File.read('help.txt')
+languages = {
+	"German": "de_DE",
+	"English": "en_US",
+	"Spanish": "es_ES",
+	"French": "fr_FR",
+	"Italian": "it_IT",
+	"Japanese": "ja_JP",
+	"Portuguese (Brazil)": "pt_BR",
+	"Chinese (simplified)": "zh_Hans"
+}
+
+
 
 
 
@@ -7,15 +17,32 @@ table_line_regex = /^\s{2,}([\w-].+?)\s{2,}(\w.+?)$/
 table_regex = /(^##[^\n]*?$)(\n(^[^\n]+?\|[^\n]+?$))/
 
 
-cli_output.gsub!(/\s*?$/, '')
-cli_output.gsub!(/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>#{x}" }
-cli_output.gsub!(table_line_regex){ "   #{$1}  |  #{$2}"}
-cli_output.gsub!(table_regex){"#{$1}\nName   |   Description\n------------|------------#{$2}"}
-cli_output.gsub!('_', '\_')
+
+languages.each do |lang, code|
+
+	`cf config --locale #{code}; cf help > help.txt`
+
+	cli_output = File.read('help.txt')
+
+	cli_output.gsub!(/\s*?$/, '')
+	cli_output.gsub!(/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>#{x}" }
+	cli_output.gsub!(table_line_regex){ "   #{$1}  |  #{$2}"}
+	cli_output.gsub!(table_regex){"#{$1}\nName   |   Description\n------------|------------#{$2}"}
+	cli_output.gsub!('_', '\_')
 
 
-title = "---\ntitle: CF CLI\n---\n\n"
+	title = "---\ntitle: CF CLI #{lang}\n---\n\n"
 
-File.open('cli_help.html.md.erb', "w") { |file| file << title }
-File.open('cli_help.html.md.erb', "a") { |file| file << cli_output }
+	File.open("cli_help_#{code}.html.md.erb", "w") { |file| file << title }
+	File.open("cli_help_#{code}.html.md.erb", "a") { |file| file << cli_output }
+
+end
+
+
+
+
+
+
+
+
 
