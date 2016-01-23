633834410990392c7bb62c907833d643883b8453
Michael Trestman <mtrestman@pivotal.io>
2016-01-22 16:11:07 -0800

    Fixes script
    [#111651002]
    
    Signed-off-by: Elena Sharma <esharma@pivotal.io>

--- format_script.rb
+++ format_script.rb
@@ -1,18 +1,20 @@
 `cf help > help.txt`
 cli_output = File.read('help.txt')
 
-cli_output.gsub! /\s*?$/, ''
-cli_output.gsub! (/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>#{x}" }
+
 
 table_line_regex = /^\s{2,}([\w-].+?)\s{2,}(\w.+?)$/
-
-cli_output.gsub!(table_line_regex){ "   #{$1}  |  #{$2}"}
-
 table_regex = /(^##[^\n]*?$)(\n(^[^\n]+?\|[^\n]+?$))/
 
-cli_output.gsub!(table_regex){"#{$1}\nName   |   Description\n------------|------------#{$2}"}
 
-chunk_regex = /(##.*?\/a>)(.*?):(.*?)(?=#)/m
+cli_output
+	.gsub!(/\s*?$/, '')
+	.gsub! (/^([^\s].*?):$/){"## <a id='#{ x = $1; $1.downcase.gsub(' ','-')}'></a>#{x}" }
+	.gsub!(table_line_regex){ "   #{$1}  |  #{$2}"}
+	.gsub!(table_regex){"#{$1}\nName   |   Description\n------------|------------#{$2}"}
+	.gsub!('_', '\_')
+
+
 title = "---\ntitle: CF CLI\n---\n\n"
 
 File.open('cli_help.html.md.erb', "w") { |file| file << title }
